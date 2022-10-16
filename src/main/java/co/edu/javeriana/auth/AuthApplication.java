4b52100356caec67c32f1a5e879aff9f3cfe07c1
kmor8 <kamilaoo88@outlook.com>
2022-10-16 18:49:39 -0500

    prueba

--- src/main/java/co/edu/javeriana/auth/AuthApplication.java
+++ src/main/java/co/edu/javeriana/auth/AuthApplication.java
@@ -21,6 +21,7 @@ public class AuthApplication {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
+			System.out.println("Esto es una prueba");
 			http.csrf().disable()
 					.addFilterAfter(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 					.authorizeRequests()
