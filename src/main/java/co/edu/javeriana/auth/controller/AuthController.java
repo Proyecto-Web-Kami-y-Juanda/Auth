25f2f5ec7db4a3c28dac52b4f70e51bb75b0b5f0
kmor8 <kamilaoo88@outlook.com>
2022-10-16 21:29:31 -0500

    funcionando

--- src/main/java/co/edu/javeriana/auth/controller/AuthController.java
+++ src/main/java/co/edu/javeriana/auth/controller/AuthController.java
@@ -1,10 +1,7 @@
 package co.edu.javeriana.auth.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import co.edu.javeriana.auth.dto.User;
 import co.edu.javeriana.auth.service.AuthService;
@@ -16,6 +13,7 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("login")
+    @CrossOrigin
     public User login(@RequestParam String user, @RequestParam String password) {
         return authService.login(user, password);
     }
